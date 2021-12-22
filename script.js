927fa28d17497936e76b08c867136ac548540e21
Carlos Caicedo <calicheccmusica@gmail.com>
2021-12-21 23:03:15 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+console.log("Page loaded!!!")
+
+var count = 70;
+
+function add1() {
+    var countElement = document.querySelector("#number");
+    console.log(countElement)
+    count++;
+    console.log(count);
+    countElement.innerText = count + "like(s)"
+}
+
+var countwo = 99;
+
+function add() {
+    var countElement = document.querySelector("#numbertwo");
+    console.log(countElement)
+    countwo++;
+    console.log(countwo);
+    countElement.innerText = countwo + "like(s)"
+}
+
+var counthree = 87;
+
+function addone() {
+    var countElement = document.querySelector("#numberthree");
+    console.log(countElement)
+    counthree++;
+    console.log(counthree);
+    countElement.innerText = counthree + "like(s)"
+}
+
+
+
+
+
